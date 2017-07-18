289bc555cd6e53f3092443779b30f2c0678703d9
Wanderrer <rwboshae@gmail.com>
2017-07-18 10:26:25 -0700

    Added Project Euler html and CSS

--- projects/project-euler/js/problem-1/multiples-of-factors.js
+++ projects/project-euler/js/problem-1/multiples-of-factors.js
@@ -12,8 +12,21 @@ function findNaiveSum(factors,upperBound) {
   return sum;
 }
 
+function findGaussSum(factors, upperBound) {
+    // for each factor
+    
+    // divide upperBound by factor
+    // floor the value
+    // Use Gauss formula
+    // multiply by divided out factor
+    // Inclusion exclusion principle.
+    // Example
+    // factors: 3, 5 , 7
+    // This would change to 3 + 5 + 7 - 3N5 - 3N7 - 5N7  + 3N5N7
+}
+
 var myFactors = ['3','5'];
 var myUpperBound = 1000;
 
 var naiveSum = findNaiveSum(myFactors,myUpperBound);
-console.log(naiveSum);
+print(naiveSum);
