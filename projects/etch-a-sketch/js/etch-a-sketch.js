ae827293e4ed694834dcf989a1cb016d84aa29fb
Rick <rwboshae@gmail.com>
2017-08-03 18:37:35 -0700

    Added Etch-A-Sketch Interface

--- projects/etch-a-sketch/js/etch-a-sketch.js
+++ projects/etch-a-sketch/js/etch-a-sketch.js
@@ -6,7 +6,7 @@ $(document).ready(function main() {
     setGridSizeByTable();
     
     // Generate New Grid
-    $('#set-grid-size-btn').click(setGridSizeByTable);
+    $('#grid-size-btn-container').click(setGridSizeByTable);
     
     // Color Grid when hovered over
     $(document).on("mouseenter", ".pixel",colorPixel);
@@ -18,7 +18,33 @@ $(document).ready(function main() {
 });
 
 function colorPixel() {
-    $(this).css("backgroundColor", "blue");
+    var color = "#0000FF";
+    percent = "0.0"
+    if (document.getElementById('red-radio-btn').checked) {
+        color = document.getElementById('red-radio-btn').value;
+    } else if (document.getElementById('blue-radio-btn').checked) {
+        color = document.getElementById('blue-radio-btn').value;
+    } else if (document.getElementById('green-radio-btn').checked) {
+        color = document.getElementById('green-radio-btn').value;
+    } else if (document.getElementById('random-radio-btn').checked) {
+        color = getRandomColor();
+    }
+    console.log(color);
+    
+    
+    if (document.getElementById('gradient-checkbox').checked){
+        percent = percent + .1;
+        shadeColor = shadeColor2(color, percent);
+        conole.log(percent);
+        console.log(shadeColor);
+        
+        $(this).css("backgroundColor", shadeColor);
+    } else {
+        $(this).css("backgroundColor", color);
+    }
+    //$(this).css("filter", 10%);
+    
+    
 }
 
 function setGridSizeByTable() {
@@ -72,4 +98,20 @@ function eraseGrid() {
     $(".pixel").remove();
     $("#display-table").remove();
     
+}
+
+function getRandomColor() {
+    var letters = '0987654321ABCDEF';
+    var color ='#';
+    
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+    
+}
+
+function shadeColor2(color, percent) {   
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
 }
