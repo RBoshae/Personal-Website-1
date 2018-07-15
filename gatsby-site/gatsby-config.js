68f3a6ba74875f158160c519be6df94817eee898
Wanderrer <rwboshae@gmail.com>
2018-07-14 17:08:10 -0700

    Trying Gatsby

---
+++ gatsby-site/gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pathPrefix: '/https://github.com/RBoshae/RBoshae.github.io',
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
