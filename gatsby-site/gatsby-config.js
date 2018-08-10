b0a02b95412302dc675ef7f5bf414eddd68bbf55
Wanderrer <rwboshae@gmail.com>
2018-08-10 16:24:13 -0700

    Removed Gatsby site

--- gatsby-site/gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  pathPrefix: '/https://github.com/RBoshae/RBoshae.github.io',
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
